e38a0a29800cedc8d8a44e6c3eab842a4d70bd27
Ad Kaium <adkaiumalikhan01@gmail.com>
2023-03-04 08:00:34 +0300

    add login/singup commponent

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LogIn.css';
+
+const LogIn = () => {
+    return (
+        <div>
+            <h1>This is login</h1>
+        </div>
+    );
+};
+
+export default LogIn;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFI3myd4tD6EIUzZu4Tr0e2dDibktwciE",
+  authDomain: "ema-jhon-simple-d5bf2.firebaseapp.com",
+  projectId: "ema-jhon-simple-d5bf2",
+  storageBucket: "ema-jhon-simple-d5bf2.appspot.com",
+  messagingSenderId: "886905455375",
+  appId: "1:886905455375:web:754120b09419b9c4c2347a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
