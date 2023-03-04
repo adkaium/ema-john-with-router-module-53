ba131416d95ef8775b5cff4923fb8ee8a8180150
Ad Kaium <adkaiumalikhan01@gmail.com>
2023-03-04 09:23:59 +0300

    create context

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css';
 
 const LogIn = () => {
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Log In</h1>
+            <form >
+                <div className='form-control'>
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' required />
+                </div>
+                <div className='form-control'>
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to ema john <Link to='/signup'>Create a New Account</Link></p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 const SignUp = () => {
     return (
-        <div>
-            <h1>This is signUP</h1>
-        </div>
+        <div className='form-container'>
+        <h1 className='form-title'>Log In</h1>
+        <form >
+            <div className='form-control'>
+            <label htmlFor="email">Email</label>
+            <input type="email" name='email' required />
+            </div>
+            <div className='form-control'>
+            <label htmlFor="password">Password</label>
+            <input type="password" name='password' required />
+            </div>
+            <div className='form-control'>
+            <label htmlFor="confirm">Confirm Password</label>
+            <input type="password" name='confirm' required />
+            </div>
+            <input className='btn-submit' type="submit" value="SignUp" />
+        </form>
+        <p>Alrady have an account? <Link to='/login'>Login</Link></p>
+    </div>
     );
 };
 
